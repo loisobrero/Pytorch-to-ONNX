c95a883a955cabd7d632ed3437cb4e9503e26c1e
Lois <loisobrero@gmail.com>
2023-07-26 12:18:09 -0700

    adding quantization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import time
+import onnx
 import onnxruntime
+from onnxruntime.quantization import quantize_dynamic
 from transformers import BertTokenizer, BertForSequenceClassification
 from model import MovieReviewClassifier
 from onnx_converter import convert_to_onnx
@@ -14,6 +16,11 @@ def synchronize_models():
 
     print("Models synchronized.")
 
+def quantize_onnx_model(onnx_model_path, quantized_model_path):
+    # Quantize the ONNX model and save to disk
+    quantized_model = quantize_dynamic(onnx_model_path, quantized_model_path)
+    print("Quantized model saved.")
+
 def run_pytorch_inference(review_text):
     # Load the pre-trained BERT model and tokenizer
     model_name = 'bert-base-uncased'
@@ -23,25 +30,18 @@ def run_pytorch_inference(review_text):
     # Tokenize the input review text
     inputs = tokenizer(review_text, return_tensors='pt')
 
-    # Run PyTorch inference and measure the runtime
+    # Run PyTorch inference
     outputs = model(**inputs)
 
     logits = outputs.logits
     predicted_class = torch.argmax(logits, dim=1).item()
     predicted_class_name = 'positive' if predicted_class == 1 else 'negative'
 
-    print(f"The PyTorch model predicted the review as: {predicted_class_name}")
     return predicted_class_name
 
-def run_onnx_inference(review_text):
+def run_onnx_inference(review_text, model_path):
     # Load the ONNX model
-    onnx_model_path = "movie_review_classifier.onnx"
-    ort_session = onnxruntime.InferenceSession(onnx_model_path)
-
-    # Print the input names and shapes of the ONNX model
-    print("Input names and shapes of the ONNX model:")
-    for input_info in ort_session.get_inputs():
-        print(f"{input_info.name}: {input_info.shape}")
+    ort_session = onnxruntime.InferenceSession(model_path)
 
     # Tokenize the input review text
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -59,12 +59,16 @@ def run_onnx_inference(review_text):
     predicted_class = torch.argmax(logits, dim=1).item()
     predicted_class_name = 'positive' if predicted_class == 1 else 'negative'
 
-    print(f"The ONNX model predicted the review as: {predicted_class_name}")
     return predicted_class_name
 
 def main():
     # Synchronize the PyTorch and ONNX models
     synchronize_models()
+
+    # Quantize the ONNX model
+    onnx_model_path = "movie_review_classifier.onnx"
+    quantized_model_path = "movie_review_classifier_quantized.onnx"
+    quantize_onnx_model(onnx_model_path, quantized_model_path)
 
     # Example review texts
     review_texts = [
@@ -75,17 +79,27 @@ def main():
     for review_text in review_texts:
         # Perform PyTorch inference
         start_time = time.time()
-        run_pytorch_inference(review_text)
+        pytorch_prediction = run_pytorch_inference(review_text)
         end_time = time.time()
         runtime = end_time - start_time
         print(f"PyTorch inference runtime: {runtime:.6f} seconds")
+        print(f"PyTorch prediction: {pytorch_prediction}")
 
         # Perform ONNX inference
         start_time = time.time()
-        run_onnx_inference(review_text)
+        prediction = run_onnx_inference(review_text, onnx_model_path)
         end_time = time.time()
         runtime = end_time - start_time
+        print(f"ONNX model predicted the review as: {prediction}")
         print(f"ONNX inference runtime: {runtime:.6f} seconds")
+
+        # Perform quantized ONNX inference
+        start_time = time.time()
+        prediction = run_onnx_inference(review_text, quantized_model_path)
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(f"Quantized ONNX model predicted the review as: {prediction}")
+        print(f"Quantized ONNX inference runtime: {runtime:.6f} seconds")
 
 if __name__ == "__main__":
     main()
